c992938aed2fe3720e7a8541c9f93c307f516a4a
Tanner Villarete <tvillarete@gmail.com>
2019-03-10 20:29:17 -0700

    Add MarkLogic Data Discovery Tool

---
+++ MarkLogic/precache-manifest.022f77b5a9dff84d2436dafd1c07d3f2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "81bd6420542d79b94815",
+    "url": "/MarkLogic/static/js/main.81bd6420.chunk.js"
+  },
+  {
+    "revision": "c77dcba955f81266ff1c",
+    "url": "/MarkLogic/static/js/1.c77dcba9.chunk.js"
+  },
+  {
+    "revision": "c86a824d81294a99aeda",
+    "url": "/MarkLogic/static/js/runtime~main.c86a824d.js"
+  },
+  {
+    "revision": "8a8414fbf7f6d8479f132dac43af31e9",
+    "url": "/MarkLogic/index.html"
+  }
+];
