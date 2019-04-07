1515e3d5fb0294e4bede7f9bab638dc21d04b38c
Tanner Villarete <tvillarete@gmail.com>
2019-04-06 18:25:00 -0700

    Add iPod/ directory
    This is my ipod-classic-js React project.

---
+++ ipod/precache-manifest.6680ac3c93fadaf616c06e41969e8d5d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "119e35478d297c807660",
+    "url": "/ipod/static/js/main.bf4b29f4.chunk.js"
+  },
+  {
+    "revision": "4319e8b4e94411c764ab",
+    "url": "/ipod/static/js/runtime~main.f1f92253.js"
+  },
+  {
+    "revision": "2fa622d558e390014822",
+    "url": "/ipod/static/js/2.532d1d61.chunk.js"
+  },
+  {
+    "revision": "7e125ff10fc7bf6126855656a69edc75",
+    "url": "/ipod/index.html"
+  }
+];
