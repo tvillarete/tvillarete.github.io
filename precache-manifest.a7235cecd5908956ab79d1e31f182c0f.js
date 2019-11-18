37b807b9df42944930c97d846f1806b6e25c4ea2
Tanner Villarete <tvillarete@gmail.com>
2019-11-17 18:45:32 -0800

    Add New Portfolio

---
+++ precache-manifest.a7235cecd5908956ab79d1e31f182c0f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48158b14d538004003a6b8a718237ebf",
+    "url": "tannerv.com/index.html"
+  },
+  {
+    "revision": "3f66a26645a33d71bfd9",
+    "url": "tannerv.com/static/js/2.eb4e04a4.chunk.js"
+  },
+  {
+    "revision": "551c72f65087f90698ce",
+    "url": "tannerv.com/static/js/main.a80b2700.chunk.js"
+  },
+  {
+    "revision": "250ab354202ffbf0abe5",
+    "url": "tannerv.com/static/js/runtime-main.73382b24.js"
+  }
+]);
