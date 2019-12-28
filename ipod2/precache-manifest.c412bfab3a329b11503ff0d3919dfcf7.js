b86f345bf8fd799f6eb822a2740002ff50a193d4
Tanner Villarete <tvillarete@gmail.com>
2019-12-27 16:33:43 -0800

    Add new version of iPod (iPod 2)

---
+++ ipod2/precache-manifest.c412bfab3a329b11503ff0d3919dfcf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c307a819eadb434bedb13587a7a407a6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "47951227496df587b709",
+    "url": "./static/js/2.e306adfc.chunk.js"
+  },
+  {
+    "revision": "ce1a861a7cb7d758383f0b724ef58ac6",
+    "url": "./static/js/2.e306adfc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc7dfe70cde8f898dc95",
+    "url": "./static/js/main.9d073b95.chunk.js"
+  },
+  {
+    "revision": "09c662aded0278f28c15",
+    "url": "./static/js/runtime-main.51e4e42a.js"
+  }
+]);
